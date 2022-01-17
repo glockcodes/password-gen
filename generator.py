4dd136239765992821014894d1805423c1bfbed3
glock <74268710+glockcodes@users.noreply.github.com>
2022-01-17 00:22:16 -0500

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,31 +1,24 @@
-# Import the random module
+# Import the sys module
+import sys
+# import the random module
 import random
+# Import the random module
+import string
 
 # Create a password generator function
 def password_generator(length, number):
-    # Create a list of characters
-    characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '}', '[', ']', '|', ':', ';', '"', '<', '>', ',', '.', '?', '/', '`', '~', ' ']
-    # Create a list of numbers
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    # Create a list of special characters
-    special_characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '}', '[', ']', '|', ':', ';', '"', '<', '>', ',', '.', '?', '/', '`', '~', ' ']
-    # Create a list of passwords
+    # Create a list to hold the passwords
     passwords = []
-    # Create a variable to hold the password
-    password = ''
-
     # Loop through the number of passwords
     for i in range(number):
-        # Loop through the length of the password
-        for j in range(length):
-            # Generate a random number
-            random_number = random.randint(0, len(characters) - 1)
-            # Add the random number to the password
-            password += characters[random_number]
-        # Add the password to the list of passwords
-        passwords.append(password)
-        # Reset the password
+        # Create a variable to hold the password
         password = ''
+        # Loop through the length of the password
+        for i in range(length):
+            # Add a random character to the password
+            password += random.choice(string.ascii_letters + string.digits)
+        # Add the password to the list
+        passwords.append(password)
     # Return the list of passwords
     return passwords
 
